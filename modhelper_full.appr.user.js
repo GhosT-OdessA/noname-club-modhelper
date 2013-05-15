ca512652432a6f6e9acf09ac76bd21b7e56bc346
GhosT-OdessA <odessky@mail.ru>
2013-05-15 16:37:47 +0300

    Update modhelper_full.appr.user.js
    откат версии до 1

--- modhelper_full.appr.user.js
+++ modhelper_full.appr.user.js
@@ -2,12 +2,12 @@
 // @name          NoNaMe-Club ModHelper
 // @namespace     NoNaMe-Club.Scripts
 // @description   Замена стандартного варианта (корень Темпа), при переносе, на выбранные форумы. Версия с проверкой на «одобреность» темы
-// @version       2.0
+// @version       1.0
 // @original author	Kaener
 // @author        Team of co-authors NNM-Club
 // @homepage      https://github.com/GhosT-OdessA/noname-club-modhelper
-// @updateURL     https://raw.github.com/GhosT-OdessA/noname-club-modhelper/master/modhelper_full.appr.meta.js
-// @downloadURL   https://raw.github.com/GhosT-OdessA/noname-club-modhelper/master/modhelper_full.appr.js
+// @updateURL     https://github.com/GhosT-OdessA/noname-club-modhelper/raw/test/modhelper_full.appr.meta.js
+// @downloadURL   https://github.com/GhosT-OdessA/noname-club-modhelper/raw/test/modhelper_full.appr.user.js
 // @include       http://*.nnm-club.ru/forum/modcp.php*
 // @include       http://nnm-club.ru/forum/modcp.php*
 // @include       https://*.nnm-club.ru/forum/modcp.php*
