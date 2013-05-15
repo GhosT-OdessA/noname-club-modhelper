78af7f4e42a889f4f8b3a38ac61299b12fa6284c
GhosT-OdessA <odessky@mail.ru>
2013-05-15 17:21:38 +0300

    Update modhelper_full.appr.meta.js
    исправлена ссылка на файл в @updateURL

--- modhelper_full.appr.meta.js
+++ modhelper_full.appr.meta.js
@@ -6,7 +6,7 @@
 // @original author	Kaener
 // @author        odessky
 // @homepage      https://github.com/GhosT-OdessA/noname-club-modhelper
-// @updateURL     https://raw.github.com/GhosT-OdessA/noname-club-modhelper/master/modhelper_full.appr.meta.js
+// @updateURL     https://github.com/GhosT-OdessA/noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
 // @include       http://*.nnm-club.ru/forum/modcp.php*
 // @include       http://nnm-club.ru/forum/modcp.php*
 // @include       https://*.nnm-club.ru/forum/modcp.php*
@@ -15,7 +15,6 @@
 // @match         http://nnm-club.ru/forum/modcp.php*
 // @match         https://*.nnm-club.ru/forum/modcp.php*
 // @match         https://nnm-club.ru/forum/modcp.php*
-// @run-at        document-start
 // ==/UserScript==
 //
 var VERSION = 2.0;
