9be1188ce200a42751fb6b410a915f829b1224cd
GhosT-OdessA <odessky@mail.ru>
2013-05-16 11:44:39 +0300

    Update modhelper_full.appr.meta.js

--- modhelper_full.appr.meta.js
+++ modhelper_full.appr.meta.js
@@ -2,7 +2,7 @@
 // @name          NoNaMe-Club ModHelper
 // @namespace     NoNaMe-Club.Scripts
 // @description   Замена стандартного варианта (корень Темпа), при переносе, на выбранные форумы. Версия с проверкой на «одобреность» темы
-// @version       2.0
+// @version       2.0.0.1
 // @original author  Kaener
 // @author        Team of co-authors NNM-Club
 // @homepage      https://github.com/GhosT-OdessA/noname-club-modhelper
