ae5a37db4f6e639528bed4f14b8cccf194437623
Siggraph <dark-hunter@live.ru>
2013-08-02 17:07:23 +0400

    доменная зона и инкремент версии

--- modhelper_full.appr.meta.js
+++ modhelper_full.appr.meta.js
@@ -2,19 +2,19 @@
 // @name          NoNaMe-Club ModHelper
 // @namespace     NoNaMe-Club.Scripts
 // @description   Замена стандартного варианта (корень Темпа), при переносе, на выбранные форумы. Версия с проверкой на «одобреность» темы
-// @version       2.0.0.5
+// @version       2.0.0.13
 // @original author	Kaener
 // @author        odessky
 // @homepage      https://github.com/GhosT-OdessA/noname-club-modhelper
 // @updateURL     https://github.com/GhosT-OdessA/noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
-// @include       http://*.nnm-club.ru/forum/modcp.php*
-// @include       http://nnm-club.ru/forum/modcp.php*
-// @include       https://*.nnm-club.ru/forum/modcp.php*
-// @include       https://nnm-club.ru/forum/modcp.php*
-// @match         http://*.nnm-club.ru/forum/modcp.php*
-// @match         http://nnm-club.ru/forum/modcp.php*
-// @match         https://*.nnm-club.ru/forum/modcp.php*
-// @match         https://nnm-club.ru/forum/modcp.php*
+// @include       http://*.nnm-club.me/forum/modcp.php*
+// @include       http://nnm-club.me/forum/modcp.php*
+// @include       https://*.nnm-club.me/forum/modcp.php*
+// @include       https://nnm-club.me/forum/modcp.php*
+// @match         http://*.nnm-club.me/forum/modcp.php*
+// @match         http://nnm-club.me/forum/modcp.php*
+// @match         https://*.nnm-club.me/forum/modcp.php*
+// @match         https://nnm-club.me/forum/modcp.php*
 // ==/UserScript==
 //
 
