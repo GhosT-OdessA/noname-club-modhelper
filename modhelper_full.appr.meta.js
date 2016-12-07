9716289f6dfd1c5e5636474b32c2f11b01cc42c6
ElSwanko <elswanko@gmail.com>
2016-12-07 17:17:03 +0400

    Update modhelper_full.appr.meta.js
    Поднял версию.

--- modhelper_full.appr.meta.js
+++ modhelper_full.appr.meta.js
@@ -2,7 +2,7 @@
 // @name          NoNaMe-Club ModHelper
 // @namespace     NoNaMe-Club.Scripts
 // @description   Замена стандартного варианта (корень Темпа), при переносе, на выбранные форумы. Версия с проверкой на «одобреность» темы
-// @version       2.1.0.0
+// @version       2.1.0.1
 // @original author    Kaener
 // @author        Team of co-authors NNM-Club
 // @homepage      https://github.com/ElSwanko/noname-club-modhelper
@@ -16,6 +16,18 @@
 // @match         http://nnmclub.to/forum/modcp.php*
 // @match         https://*.nnmclub.to/forum/modcp.php*
 // @match         https://nnmclub.to/forum/modcp.php*
+// @include       http://*.nnm-club.me/forum/modcp.php*
+// @include       http://nnm-club.me/forum/modcp.php*
+// @include       https://*.nnm-club.me/forum/modcp.php*
+// @include       https://nnm-club.me/forum/modcp.php*
+// @match         http://*.nnm-club.me/forum/modcp.php*
+// @match         http://nnm-club.me/forum/modcp.php*
+// @match         https://*.nnm-club.me/forum/modcp.php*
+// @match         https://nnm-club.me/forum/modcp.php*
+// @include       http://dsenxis5txr4zbxe.onion/forum/modcp.php*
+// @include       https://dsenxis5txr4zbxe.onion/forum/modcp.php*
+// @match         http://dsenxis5txr4zbxe.onion/forum/modcp.php*
+// @match         https://dsenxis5txr4zbxe.onion/forum/modcp.php*
 // @grant         none
 // ==/UserScript==
 //
