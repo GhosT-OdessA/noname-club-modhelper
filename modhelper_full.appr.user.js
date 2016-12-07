59dece8842d69cecce0a8e7f2ac341f7cdcc7499
ElSwanko <elswanko@gmail.com>
2016-12-07 12:52:14 +0400

    Update modhelper_full.appr.user.js

--- modhelper_full.appr.user.js
+++ modhelper_full.appr.user.js
@@ -5,9 +5,9 @@
 // @version       2.1.0.0
 // @original author    Kaener
 // @author        Team of co-authors NNM-Club
-// @homepage      https://github.com/GhosT-OdessA/noname-club-modhelper
-// @updateURL     https://github.com/GhosT-OdessA/noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
-// @downloadURL   https://github.com/GhosT-OdessA/noname-club-modhelper/raw/master/modhelper_full.appr.user.js
+// @homepage      https://github.com/ElSwanko/noname-club-modhelper
+// @updateURL     https://github.com/ElSwanko/noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
+// @downloadURL   https://github.com/ElSwanko/noname-club-modhelper/raw/master/modhelper_full.appr.user.js
 // @include       http://*.nnmclub.to/forum/modcp.php*
 // @include       http://nnmclub.to/forum/modcp.php*
 // @include       https://*.nnmclub.to/forum/modcp.php*
