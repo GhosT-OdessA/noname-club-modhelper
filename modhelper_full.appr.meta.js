13a9cec8bacb79cdbcda303eeffa17d0b85a77c6
ElSwanko <elswanko@gmail.com>
2016-12-08 13:36:57 +0400

    Update modhelper_full.appr.meta.js
    Поднял версию.

--- modhelper_full.appr.meta.js
+++ modhelper_full.appr.meta.js
@@ -1,37 +1,37 @@
 // ==UserScript==
-// @name          NoNaMe-Club ModHelper
-// @namespace     NoNaMe-Club.Scripts
-// @description   Замена стандартного варианта (корень Темпа), при переносе, на выбранные форумы. Версия с проверкой на «одобреность» темы
-// @version       2.1.0.2
-// @original author    Kaener
-// @author        Team of co-authors NNM-Club
-// @homepage      https://github.com/ElSwanko/noname-club-modhelper
-// @updateURL     https://github.com/ElSwanko/noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
-// @downloadURL   https://github.com/ElSwanko/noname-club-modhelper/raw/master/modhelper_full.appr.user.js
-// @include       http://*.nnmclub.to/forum/modcp.php*
-// @include       http://nnmclub.to/forum/modcp.php*
-// @include       https://*.nnmclub.to/forum/modcp.php*
-// @include       https://nnmclub.to/forum/modcp.php*
-// @match         http://*.nnmclub.to/forum/modcp.php*
-// @match         http://nnmclub.to/forum/modcp.php*
-// @match         https://*.nnmclub.to/forum/modcp.php*
-// @match         https://nnmclub.to/forum/modcp.php*
-// @include       http://*.nnm-club.me/forum/modcp.php*
-// @include       http://nnm-club.me/forum/modcp.php*
-// @include       https://*.nnm-club.me/forum/modcp.php*
-// @include       https://nnm-club.me/forum/modcp.php*
-// @match         http://*.nnm-club.me/forum/modcp.php*
-// @match         http://nnm-club.me/forum/modcp.php*
-// @match         https://*.nnm-club.me/forum/modcp.php*
-// @match         https://nnm-club.me/forum/modcp.php*
-// @include       http://nnm-club.i2p.onion/forum/modcp.php*
-// @include       https://nnm-club.i2p.onion/forum/modcp.php*
-// @match         http://nnm-club.i2p.onion/forum/modcp.php*
-// @match         https://nnm-club.i2p.onion/forum/modcp.php*
-// @include       http://nnmclub5toro7u65.onion/forum/modcp.php*
-// @include       https://nnmclub5toro7u65.onion/forum/modcp.php*
-// @match         http://nnmclub5toro7u65.onion/forum/modcp.php*
-// @match         https://nnmclub5toro7u65.onion/forum/modcp.php*
-// @grant         none
+// @name            NoNaMe-Club ModHelper
+// @namespace       NoNaMe-Club.Scripts
+// @description     Замена стандартного варианта (корень Темпа) при переносе на профильные форумы. Версия с проверкой на «одобреность» темы.
+// @version         2.1.0.3
+// @original author Kaener
+// @author          Team of co-authors NNM-Club
+// @homepage        https://github.com/ElSwanko/noname-club-modhelper
+// @updateURL       https://github.com/ElSwanko/noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
+// @downloadURL     https://github.com/ElSwanko/noname-club-modhelper/raw/master/modhelper_full.appr.user.js
+// @include         http://*.nnmclub.to/forum/modcp.php*
+// @include         http://nnmclub.to/forum/modcp.php*
+// @include         https://*.nnmclub.to/forum/modcp.php*
+// @include         https://nnmclub.to/forum/modcp.php*
+// @match           http://*.nnmclub.to/forum/modcp.php*
+// @match           http://nnmclub.to/forum/modcp.php*
+// @match           https://*.nnmclub.to/forum/modcp.php*
+// @match           https://nnmclub.to/forum/modcp.php*
+// @include         http://*.nnm-club.me/forum/modcp.php*
+// @include         http://nnm-club.me/forum/modcp.php*
+// @include         https://*.nnm-club.me/forum/modcp.php*
+// @include         https://nnm-club.me/forum/modcp.php*
+// @match           http://*.nnm-club.me/forum/modcp.php*
+// @match           http://nnm-club.me/forum/modcp.php*
+// @match           https://*.nnm-club.me/forum/modcp.php*
+// @match           https://nnm-club.me/forum/modcp.php*
+// @include         http://nnm-club.i2p.onion/forum/modcp.php*
+// @include         https://nnm-club.i2p.onion/forum/modcp.php*
+// @match           http://nnm-club.i2p.onion/forum/modcp.php*
+// @match           https://nnm-club.i2p.onion/forum/modcp.php*
+// @include         http://nnmclub5toro7u65.onion/forum/modcp.php*
+// @include         https://nnmclub5toro7u65.onion/forum/modcp.php*
+// @match           http://nnmclub5toro7u65.onion/forum/modcp.php*
+// @match           https://nnmclub5toro7u65.onion/forum/modcp.php*
+// @grant           none
 // ==/UserScript==
 //
