93e4eb66585426366c91dac8b1f398456898e0a6
ElSwanko <elswanko@gmail.com>
2016-12-14 11:19:17 +0400

    Update modhelper_full.appr.meta.js
    Поднял версию.

--- modhelper_full.appr.meta.js
+++ modhelper_full.appr.meta.js
@@ -2,12 +2,12 @@
 // @name            NoNaMe-Club ModHelper
 // @namespace       NoNaMe-Club.Scripts
 // @description     Замена стандартного варианта (корень Темпа) при переносе на профильные форумы. Версия с проверкой на «одобреность» темы.
-// @version         2.1.0.4
+// @version         2.1.0.5
 // @original author Kaener
 // @author          Team of co-authors NNM-Club
-// @homepage        https://github.com/ElSwanko/noname-club-modhelper
-// @updateURL       https://github.com/ElSwanko/noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
-// @downloadURL     https://github.com/ElSwanko/noname-club-modhelper/raw/master/modhelper_full.appr.user.js
+// @homepage        https://github.com/GhosT-OdessA//noname-club-modhelper
+// @updateURL       https://github.com/GhosT-OdessA//noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
+// @downloadURL     https://github.com/GhosT-OdessA//noname-club-modhelper/raw/master/modhelper_full.appr.user.js
 // @include         http://*.nnmclub.to/forum/modcp.php*
 // @include         http://nnmclub.to/forum/modcp.php*
 // @include         https://*.nnmclub.to/forum/modcp.php*
