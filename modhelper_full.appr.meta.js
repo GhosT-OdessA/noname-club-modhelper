79b1d23a710c043d324533862bbe7e25049df04d
Nicolas Angelov <nikovolt220@gmail.com>
2017-12-20 22:47:43 +0200

    Update modhelper_full.appr.meta.js

--- modhelper_full.appr.meta.js
+++ modhelper_full.appr.meta.js
@@ -3,13 +3,14 @@
 // @name            NoNaMe-Club ModHelper
 // @namespace       NoNaMe-Club.Scripts
 // @description     Замена стандартного варианта (корень Темпа) при переносе на профильные форумы. Версия с проверкой на «одобреность» темы.
-// @version         2.1.0.8
+// @version         2.1.0.9
 // @original author Kaener
 // @author          Team of co-authors NNM-Club
 // @homepage        https://github.com/GhosT-OdessA/noname-club-modhelper
 // @updateURL       https://github.com/GhosT-OdessA/noname-club-modhelper/raw/master/modhelper_full.appr.meta.js
 // @downloadURL     https://github.com/GhosT-OdessA/noname-club-modhelper/raw/master/modhelper_full.appr.user.js
 // @match        *://*.nnmclub.to/forum/modcp.php*
+// @match        *://*.nnmclub.lib/forum/modcp.php*
 // @match        *://*.nnm-club.name/forum/modcp.php*
 // @match        *://*.nnm-club.me/forum/modcp.php*
 // @match        *://*.nnm-club.i2p.onion/forum/modcp.php*
